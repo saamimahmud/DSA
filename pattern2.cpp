27d8f3520647a33e3bf6285e5850ad5404a4574e
saamimahmud <saamimahmud123@gmail.com>
2025-02-20 00:07:17 +0530

    taking no. of rows from user

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,11 +1,32 @@
-#include<iostream>
+/*
+QUESTION-> print the following pattern :
 
+* 
+* *
+* * *
+* * * *
+* * * * *
+
+
+*/
+
+#include<iostream>
+using namespace std;
     void pattern2(int n){
-        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
     }
 
 
 
 int main(){
+    int n;
+    cout<<"Enter the number of rows : "<<endl;
+    cin>>n;
+    pattern2(5);
     return 0;
 }
