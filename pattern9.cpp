c78b691988d13d3c68eec8c98fabec5934468fc4
saamimahmud <saamimahmud123@gmail.com>
2025-02-24 23:00:30 +0530

    dsa striver pattern8

---
+++ pattern9.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+ 
+void pattern9(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=2*i-1;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+        
+    }
+    
+    
+}
+void pattern8(int n){
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=i-1;k++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*n-(2*i-1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+
+int main(){
+int n;
+cout<<"Enter the no. o rows: ";
+cin>>n;
+pattern9(5);
+pattern8(5);
+
+    return 0;
+}
