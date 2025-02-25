8052dc2755335bf51abef7a3fa234210240c2786
saamimahmud <saamimahmud123@gmail.com>
2025-02-25 23:19:21 +0530

    final commit

--- pattern12.cpp
+++ pattern12.cpp
@@ -1,45 +1,44 @@
 /*
 QUESTION-> create the following pattern :
 
-
-    
-    
+1          1
+12        21
+123      321
+1234    4321
+12345  54321
+123456654321
 
 */
 
 #include<iostream>
 using namespace std;
 
-void pattern10(int n){
+void pattern12(int n){
+    
     for(int i=1;i<=n;i++){
+        int m=i;
         for(int j=1;j<=i;j++){
             cout<<j;
         }    
     
         for(int j=1;j<=n-i;j++){
-            cout<<"*";
+            cout<<" ";
         }
         for(int j=1;j<=n-i;j++){
-            cout<<"*";
+            cout<<" ";
         }
         for(int j=1;j<=i;j++){
            
-            cout<<j;
-            
+            cout<<m;
+            m=m-1;            
         }  
-        
         cout<<endl;
     }
 }
-
-
-
 int main(){
 int n;
 cout<<"Enter the no. of rows: "<<endl;
 cin>>n;
-pattern10(n);
-
-
+pattern12(n);
     return 0;
 }
