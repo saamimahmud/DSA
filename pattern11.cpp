3317dd1ec0091e60d6d5f17c77ff51e27f824400
saamimahmud <saamimahmud123@gmail.com>
2025-02-25 22:45:29 +0530

    final commit

--- pattern11.cpp
+++ pattern11.cpp
@@ -13,14 +13,16 @@ using namespace std;
 void pattern12(int n){
     int start=1;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            if(i%2==0){
+        if(i%2==0){
                 start=0;
             }
             else{
                 start=1;
             }
+        for(int j=1;j<=i;j++){
+            
             cout<<start;
+            start=1-start;
         }
         cout<<endl;
     }
