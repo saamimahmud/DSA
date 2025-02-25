971e1d7d64a1f70bb72642034075702245ecd424
saamimahmud <saamimahmud123@gmail.com>
2025-02-26 00:30:20 +0530

    final commit

---
+++ pattern16.cpp
@@ -0,0 +1,34 @@
+/*
+QUESTION-> create the following pattern :
+
+A 
+B B
+C C C
+D D D D
+E E E E E
+
+*/
+
+#include<iostream>
+using namespace std;
+
+void pattern15(int n){
+     char m='A';
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=i;j++){
+            cout<<m<<" ";
+            
+            
+        }   
+        m=m+1; 
+        cout<<endl;
+    }
+}
+int main(){
+int n;
+cout<<"Enter the no. of rows: "<<endl;
+cin>>n;
+pattern15(n);
+    return 0;
+}
