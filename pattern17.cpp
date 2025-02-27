fbfa5f8fdeadbcae74e369123f7db84cde2ae823
saamimahmud <saamimahmud123@gmail.com>
2025-02-27 23:24:15 +0530

    initial commit

---
+++ pattern17.cpp
@@ -0,0 +1,26 @@
+/*
+QUESTION-> create the following pattern :
+
+     A     
+    ABA    
+   ABCBA   
+  ABCDCBA  
+ ABCDEDCBA 
+ABCDEFEDCBA
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+void pattern17(int n){
+
+}
+
+
+int main (){
+int n;
+
+    return 0;
+}
