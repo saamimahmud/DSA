86efc61f9b8f29c8f826b75427adda73fd8ed99e
saamimahmud <saamimahmud123@gmail.com>
2025-02-27 23:51:34 +0530

    final commit

--- pattern17.cpp
+++ pattern17.cpp
@@ -7,31 +7,35 @@ QUESTION-> create the following pattern :
   ABCDCBA  
  ABCDEDCBA 
 ABCDEFEDCBA
-
-
 */
-
 #include<iostream>
 using namespace std;
 
 void pattern17(int n){
-    char var='A';
+    
     for(int i=1;i<=n;i++){
+        char var='A';
         for(int j=1;j<=n-i+1;j++){
-            cout<<"*";
+            cout<<" ";
         }
-        for(int k=1;k<=n;k++){
+        for(int k=1;k<=2*i-1;k++){
             cout<<var;
-            var++;
+            
+            if(k>=i){
+                var--;
+            }
+            else{
+                var++;
+            }
         }
         cout<<endl;
     }
-    
 }
-
 
 int main (){
 int n;
-pattern17(5);
+cout<<"Enter the number of rows : ";
+cin>>n;
+pattern17(n);
     return 0;
 }
