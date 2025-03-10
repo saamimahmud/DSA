473b4cb322b0dd295cf06790e4126e103c59f3ea
saamimahmud <saamimahmud123@gmail.com>
2025-03-11 02:57:40 +0530

    first commit

---
+++ Armstrong_Number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void CheckArm(int n){
+    int last_digit;
+    while (n>0)
+    {
+        last_digit=n%10;
+    }
+    
+    
+
+}
+
+int main(){
+
+    return 0;
+}
