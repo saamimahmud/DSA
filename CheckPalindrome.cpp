c1a6aeab2b6f7c5bb9259ab99ae20b1e9814f859
saamimahmud <saamimahmud123@gmail.com>
2025-03-11 02:17:02 +0530

    first commit

---
+++ CheckPalindrome.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void palindrome(int n){
+    int last_digit;
+    int rev_num=0;
+    while(n>0){
+        last_digit=n%10;
+        rev_num=rev_num*10+last_digit;
+
+    }
+    if(last_digit==n){
+        cout<<"The number is a palindrome...";
+    }
+    else{
+        cout<<"The number is not a palindrome...";
+    }
+}
