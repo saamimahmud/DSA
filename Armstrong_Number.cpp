9710b5779835fd271a8e085954513479c8b948a5
saamimahmud <saamimahmud123@gmail.com>
2025-03-11 03:15:30 +0530

    final commit

--- Armstrong_Number.cpp
+++ Armstrong_Number.cpp
@@ -1,17 +1,33 @@
 #include<iostream>
 using namespace std;
-void CheckArm(int n){
+auto n_From_User(){
+    int n;
+    cout<<"enter a number : ";
+    cin>>n;
+    return n;
+}
+void IsArmNum(int n){
     int last_digit;
+    int checkNum=0;
+    int t=n;
     while (n>0)
     {
         last_digit=n%10;
+        checkNum=  checkNum + last_digit *last_digit*last_digit;
+        n=n/10;
+    }
+    cout<<"sum of cube of digits =  "<<checkNum<<endl;
+    if (checkNum==t)
+    {
+        cout<<"it is an Armstrong number ";
+    }else{
+        cout<<"it is not an Armstrong number ";
     }
     
-    
-
 }
 
 int main(){
-
+    int n=n_From_User();
+    IsArmNum(n);
     return 0;
 }
