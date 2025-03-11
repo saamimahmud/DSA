6a319d14f7817adf0887bcb8b675d8bb0771d757
saamimahmud <saamimahmud123@gmail.com>
2025-03-11 13:07:45 +0530

    final commit

--- Print_Divisors.cpp
+++ Print_Divisors.cpp
@@ -1,10 +1,22 @@
+/*
+A program to print all the divisors of a number.
+striver DSA sheet
+*/
 #include<iostream>
 using namespace std;
 void Print_Divisors(int n){
-    for (int i = 0; i <=n; i++)
+    for (int i = 2; i <=n; i++)
     {
         if (n%i==0){
-            cout<<i;
+            cout<<i<<endl;
         }
     }   
+}
+int main(){
+int n;
+cout<<"Enter a number : "<<endl;
+cin>>n;
+cout<<"The divisors of "<<n<<" are : "<<endl;
+Print_Divisors(n);
+    return 0;
 }
