9eb0088301aed75980f5d229545307e335d192ce
saamimahmud <saamimahmud123@gmail.com>
2025-03-11 14:52:23 +0530

    updated the code with an optimized function with better tc

--- Print_Divisors.cpp
+++ Print_Divisors.cpp
@@ -1,10 +1,11 @@
 /*
 A program to print all the divisors of a number.
 striver DSA sheet
+CODE 1 with TC -> O(N)
 */
 #include<iostream>
 using namespace std;
-void Print_Divisors(int n){
+void Print_Divisors_1(int n){
     if(n==0){
             cout<<n<<endl;
         }
@@ -17,11 +18,30 @@ void Print_Divisors(int n){
         
     }   }
 }
+/*
+CODE 2 with TC -> O(log2(n))
+*/
+void Print_Divisors_2(int n){
+    if(n==0){
+            cout<<n<<endl;
+        }
+    else{
+        for (int i = 1; i*i<=n; i++)
+    {
+        if (n%i==0){
+            cout<<i<<endl;
+            if(n/i!=i){
+            cout<<n/i<<endl;
+        }
+        }    
+    }   }
+}
 int main(){
 int n;
 cout<<"Enter a number : "<<endl;
 cin>>n;
 cout<<"The divisors of "<<n<<" are : "<<endl;
-Print_Divisors(n);
+Print_Divisors_1(n);
+Print_Divisors_2(n);
     return 0;
 }
