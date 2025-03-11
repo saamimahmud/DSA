6709ffae73e970efdbdf0f80a940c542672c9ca9
saamimahmud <saamimahmud123@gmail.com>
2025-03-11 13:17:42 +0530

    final commit

--- Print_Divisors.cpp
+++ Print_Divisors.cpp
@@ -5,12 +5,17 @@ striver DSA sheet
 #include<iostream>
 using namespace std;
 void Print_Divisors(int n){
-    for (int i = 2; i <=n; i++)
+    if(n==0){
+            cout<<n<<endl;
+        }
+    else{
+        for (int i = 1; i <=n; i++)
     {
         if (n%i==0){
             cout<<i<<endl;
         }
-    }   
+        
+    }   }
 }
 int main(){
 int n;
