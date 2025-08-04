9b9ae98f580352a1807817b9260eeb2ba5799d18
saamimahmud <saamimahmud123@gmail.com>
2025-08-04 18:23:36 +0530

    new commit

--- GCD.cpp
+++ GCD.cpp
@@ -1,9 +1,13 @@
+/*
+GCD Brute Force Method
+*/
+
 #include<iostream>
 using namespace std;
 
 int GCD(int a,int b){
     int gcd=0;
-    for(int i=1;i<=a;i++){
+    for(int i=1;i<=min(a,b);i++){
         if(a%i==0&&b%i==0){
             gcd=i;
         }
