45eb835e845321982ab46d1e3dc796d931730742
saamimahmud <saamimahmud123@gmail.com>
2025-08-04 17:25:49 +0530

    New commit

---
+++ GCD.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int GCD(int a,int b){
+    int gcd=0;
+    for(int i=1;i<=a;i++){
+        if(a%i==0&&b%i==0){
+            gcd=i;
+        }
+    }
+    return gcd;
+    
+}
+int main(){
+    int a=5;
+    int b=25;
+    int result = GCD(a,b);
+    cout<<result;
+    return 0;
+}
