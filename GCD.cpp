284a3946ac4882d2ca337210fc9865e8cf7b0969
saamimahmud <saamimahmud123@gmail.com>
2025-08-06 22:53:47 +0530

    new commit

--- GCD.cpp
+++ GCD.cpp
@@ -4,7 +4,7 @@ GCD Brute Force Method
 
 #include<iostream>
 using namespace std;
-
+//brute force method
 int GCD(int a,int b){
     int gcd=0;
     for(int i=1;i<=min(a,b);i++){
@@ -12,13 +12,25 @@ int GCD(int a,int b){
             gcd=i;
         }
     }
-    return gcd;
-    
+    return gcd;   
+}
+//better approach
+void GCD2(int a,int b){
+    for(int i=min(a,b);i>=1;i--){
+        int gcd=0;
+        if(a%i==0&&b%i==0){
+            gcd=i;
+            cout<<gcd;
+            break;
+        }
+    }
 }
 int main(){
     int a=5;
     int b=25;
     int result = GCD(a,b);
-    cout<<result;
+    cout<<result<<endl;
+    GCD2(25,5);
     return 0;
 }
+
