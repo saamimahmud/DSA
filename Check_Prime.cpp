aba378c655f45df3f23226c9fb547ff6dfb8ddb3
saamimahmud <saamimahmud123@gmail.com>
2025-08-13 02:34:02 +0530

    new commits

--- Check_Prime.cpp
+++ Check_Prime.cpp
@@ -9,18 +9,34 @@ void Check_Prime1(int n){
         }
     }
     if (count>0){
-        cout<<"Number is not Prime";
+        cout<<"Number is not Prime"<<endl;
     }
     else {
-        cout<<"Number is Prime.";
+        cout<<"Number is Prime."<<endl;
+    }
+}
+//Optimal approach
+void Check_Prime2(int n){
+    int count=0;
+    for(int i=2;i*i<=n;i++){
+        if (n%i==0)
+        {
+            count =count+1;
+        }
+    }
+    if(count>0){
+        cout<<"Number is not Prime"<<endl;
+    }
+    else{
+        cout<<"Number is  Prime"<<endl;
     }
 }
 
 int main(){
 int n;
-cout<<"Enter the number you want to check for Prime :";
+cout<<"Enter the number you want to check for Prime :"<<endl;
 cin>>n;
 Check_Prime1(n);
-
+Check_Prime2(n);
 return 0;
 }
