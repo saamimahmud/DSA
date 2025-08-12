345b5b485f8c455674486c0f8d1cc3f324d7b99e
saamimahmud <saamimahmud123@gmail.com>
2025-08-12 21:07:50 +0530

    new commit

--- GCD.cpp
+++ GCD.cpp
@@ -25,12 +25,28 @@ void GCD2(int a,int b){
         }
     }
 }
+//optimal approach
+void GCD3(int a,int b){
+    int gcd=0;
+    int left_var;
+    int right_var;
+    int result;
+    left_var=max(a,b);
+    right_var=min(a,b);
+    while(a!=0&&b!=0){
+        
+        a=max(a,b)-min(a,b);
+        b=min(a,b);
+    }
+    cout<<a,b;
+}
 int main(){
     int a=5;
     int b=25;
     int result = GCD(a,b);
     cout<<result<<endl;
     GCD2(25,5);
+    GCD3(25,5);
     return 0;
 }
 
