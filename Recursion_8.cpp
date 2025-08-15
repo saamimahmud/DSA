ec8c592d90efad18b1a3e5b8be16498472503979
saamimahmud <saamimahmud123@gmail.com>
2025-08-15 17:22:59 +0530

    added func tocheck palindrome using Recursion

--- Recursion_8.cpp
+++ Recursion_8.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //Check if a string is a palindrome or not;
 
-string Check_Palindrome(string s){
+string Check_Palindrome(string s){//Normal loop approach
 int start=0;
 int end= s.length()-1;
     while (start<=end&&end>=start)
@@ -21,6 +21,17 @@ int end= s.length()-1;
        
 }
 
+string CheckPalindrome_rec(string s,int start,int end){
+    if(start<=end&&end>=start){
+        if(s[start]==s[end]){
+            CheckPalindrome_rec(s,start+1,end-1);
+        }else {
+            return "Not a palindrom.";
+        }
+    }
+    return "The input string is a palindrome.";
+}
+
 
 
 int main(){
@@ -30,6 +41,7 @@ cin>>s;
 
   cout<<Check_Palindrome(s);
 
+  cout<<CheckPalindrome_rec(s,0,s.length()-1);
     
 
     return 0;
