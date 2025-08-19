66403cb16ad9889ae6f5b42caf81d7ef9126af77
saamimahmud <saamimahmud123@gmail.com>
2025-08-19 14:51:40 +0530

    new commits

--- Recursion_9.cpp
+++ Recursion_9.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
-//Fibonacci series ..... 0 1 1 2 3 5 8 13
+//Fibonacci series ..... 0 1 1 2 3 5 8 13;
 void Fibo(int x){
-    int arr[x];
+int arr[x];
 arr[0]=0;
 arr[1]=1;
 int i=0;
@@ -21,27 +21,29 @@ void PrintSeries(int arr[],int x){
         cout<<arr[i];
     }
 }
-void Fib_Rec(int x,int p1){
-    int arr[x];
-    int p2=p1+1;
-    int p3=0;
-    arr[p1]=0;
-    arr[p2]=1;
-    if(p3<=x){
-        p3=p2+1;
-        arr[p3]=arr[p1]+arr[p2];
-        Fib_Rec(x,p1+1);
+void Fib_Rec(int x,int p1,int p2,int count){
+    
+    
+   int p3=p1+p2;
+   if (p1==0&&p2==1){
+    cout<<p1;
+   cout<<p2;
+   }
+    if(count<x){
+   
+   
+   cout<<p3;
+   count=count+1;;
+   
+   Fib_Rec(x,p2,p3,count);
     }
-
-   for(int i=0;i<=x;i++){
-       cout<<arr[i];
-    }
+   
 }
 int main(){
 int x=5;
 Fibo(x);
 cout<<endl;
-Fib_Rec(x,0);
+Fib_Rec(x,0,1,1);
 
     return 0;
 }
