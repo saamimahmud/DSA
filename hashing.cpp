bb17ec9c3952fb9d27cdcf878879f8a80e1e6b0e
saamimahmud <saamimahmud123@gmail.com>
2025-08-20 02:48:20 +0530

    new commits

--- hashing.cpp
+++ hashing.cpp
@@ -13,38 +13,40 @@
 using namespace std;
 
 void hashing(int arr[],int hash[],int n){
-    int p1=0;
     int length=n-1;
-    int count=0;
-    hash[12]=0;
-    //for(int i=0;i<12;i++){
-      //      hash[i]=0;
-       //}
-    for(int i=0;i<=length;i++){
-        hash[arr[i]]+=1;
-        
     
+    for(int i=0;i<=n;i++){
+        hash[arr[i]]++;
+        
     }
-   // for(int i=0;i<=11;i++){
-    //    cout<<hash[i];
-   // }
-  // cout<<hash[3];
+ 
 }
 
 
 
 int main(){
-int arr[]={1,3,2,1,3};
-int hash[13];
-int n=5;
-cout<<n<<endl;
+int hash[13]={0};
+int n;
+cout<<"Enter the number of elements you would like in the array : "<<endl;
+cin>>n;
+
+int arr[n];
+
+
+cout<<" Enter the array elements : "<<endl;
+for(int i=0;i<=n;i++){
+    cin>>arr[i];
+}
 hashing(arr,hash,n);
+cout<< "Enter number of queries you would like : "<<endl;
+
 int q;
 cin>>q;
+cout<< "Enter queries now  : "<<endl;
 while(q--){
     int num;
     cin>>num;
-    cout<<hash[num]<<endl;
+    cout<<"Frequency of the element "<<num<<" is equal to " <<hash[num];
 }
     return 0;
 }
