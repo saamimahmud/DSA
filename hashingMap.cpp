e73e5621bb046a79a6df65b013d7021a93aed343
saamimahmud <saamimahmud123@gmail.com>
2025-08-21 02:18:20 +0530

    new commitss

--- hashingMap.cpp
+++ hashingMap.cpp
@@ -2,18 +2,11 @@
 #include<map>
 using namespace std;
 
-void Hmap(int arr[],int n){
-    map<int,int> mpp;
-    for(int i=0;i<=n;i++){
-        mpp[arr[i]]+=1;
-    }
+
     
 
-
-}
-
-
 int main(){
+map<int,int> mpp;
 int n;
 cout<<"Enter the number of elements you would like to have as input : "<<endl;
 cin>>n;
@@ -22,6 +15,21 @@ cout<<"Now enter the elements : "<<endl;
 for(int i=0;i<=n;i++){
     cin>>arr[i];
 }
+//precompute;
+for(int i=0;i<=n;i++){
+    mpp[arr[i]]+=1;
+}
+int q;
+cout<<"Enter the number of queries you would like to ask : "<<endl;
+cin>>q;
+int ask;
+while(q--){
+    cout<<"Enter the element whose frequency you would like to know : "<<endl;
+    cin>>ask;
+    cout<<ask<<" -> "<<mpp[ask] <<endl;;
+}
+
+
 
     return 0;
 
