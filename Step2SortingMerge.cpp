6cd8eb89f714332c4d1be8bc2db6c38d4901507b
saamimahmud <saamimahmud123@gmail.com>
2025-08-23 00:51:02 +0530

    new commits

---
+++ Step2SortingMerge.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void Merge(vector<int> arr,int low,int mid,int high){
+vector<int> temp;
+int left=low;
+int right=mid+1;
+while(left<mid&&right<high){
+    if(arr[left]<=arr[right]){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    else{
+        temp.push_back(arr[right]);
+        right++;
+    }
+}
+while(left<mid){
+temp.push_back(arr[left]);
+}
+while(right<high){
+temp.push_back(arr[right]);
+}
+
+}
+
+
+void MergeSort(vector <int> arr,int low,int high){
+
+if(low==high){
+    return;
+}
+int mid=(low+high)/2;
+MergeSort(arr,low,mid);
+MergeSort(arr,mid+1,high);
+Merge(arr,low,mid,high);
+}
+
+
+
+
+
+
+int main(){
+int n;
+cout<<"Enter the size of array : "<<endl;
+cin>>n;
+vector<int> arr;
+cout<<"Enter the elements of the array"<<endl;
+for(int i=0;i<n;i++){
+cin>>arr[i];
+}
+MergeSort(arr,0,4);
+cout<<endl;
+for(int i=0;i<n;i++){
+cout<<arr[i]<<" ";
+}
+
+    return 0;
+
+}
