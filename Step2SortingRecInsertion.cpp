e2c66b868772df1e6901a2bbb5e5ce8598688f14
saamimahmud <saamimahmud123@gmail.com>
2025-08-24 00:10:17 +0530

    new commits

--- Step2SortingRecInsertion.cpp
+++ Step2SortingRecInsertion.cpp
@@ -1,21 +1,33 @@
 #include<iostream>
 using namespace std;
+//T.C -> Best - O(n) otherwise - O(n*n);
+void Insert(int arr[],int j){
 
-void RecInsert(int arr[],int j,int n){
-if(j==n){return;
+    if(j>0 &&arr[j]<arr[j-1]){
+        swap(arr[j],arr[j-1]);
+    }
+    Insert(arr,j-1);
 }
-if(arr[j]<arr[j-1]){
-    swap(arr[j],arr[j-1]);
 
-}
-RecInsert(arr,j+1,n);
+
+void Rec(int arr[],int j,int n){
+    if(j==n){
+        return ;
+    }
+    Insert(arr,j);
+    Rec(arr,j+1,n);
 
 }
 
 
 int main(){
 
-
+int arr[]={3,1,4,2,5};
+int n=5;
+Rec(arr,1,n);
+for(int i=0;i<=n-1;i++){
+    cout<<arr[i]<<" ";
+}
 
     return 0;
 }
