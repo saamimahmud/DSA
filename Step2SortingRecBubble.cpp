eed24a470d4bcf208c2a1b9d69f2ddd4e6f09407
saamimahmud <saamimahmud123@gmail.com>
2025-08-23 23:21:26 +0530

    new commits

--- Step2SortingRecBubble.cpp
+++ Step2SortingRecBubble.cpp
@@ -2,21 +2,26 @@
 using namespace std;
 
 void RecBubble(int arr[],int j,int n){
-int k=j;
-if(j<=n-1){
-
-    if(arr[j]<arr[j-1]){
-        swap(arr[j],arr[j-1]);
-        RecBubble(arr,j+1,n);
-    }
-RecBubble(arr,k+1,n);
+if(n==1){return ;
 }
+if(j<n-1){
+
+    if(arr[j]>arr[j+1]){
+        swap(arr[j],arr[j+1]);
+       
+    }
+     RecBubble(arr,j+1,n);
+}
+else{
+    RecBubble(arr,0,n-1);
+}
+
 }
 
 int main(){
 int arr[]={3,1,4,2,5};
 int n=5;
-RecBubble(arr,1,n);
+RecBubble(arr,0,n);
 for(int i=0;i<=n-1;i++){
     cout<<arr[i]<<" ";
 }
