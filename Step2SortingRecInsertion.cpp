0d4f267207f75a25e0addf69465c1f5d28f8283c
saamimahmud <saamimahmud123@gmail.com>
2025-08-23 23:37:23 +0530

    new commits

--- Step2SortingRecInsertion.cpp
+++ Step2SortingRecInsertion.cpp
@@ -1,3 +1,21 @@
 #include<iostream>
 using namespace std;
 
+void RecInsert(int arr[],int j,int n){
+if(j==n){return;
+}
+if(arr[j]<arr[j-1]){
+    swap(arr[j],arr[j-1]);
+
+}
+RecInsert(arr,j+1,n);
+
+}
+
+
+int main(){
+
+
+
+    return 0;
+}
