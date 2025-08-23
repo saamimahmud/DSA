fba07136fe5bee313d89b7e10016123f89e34db6
saamimahmud <saamimahmud123@gmail.com>
2025-08-24 05:27:07 +0530

    new commits

---
+++ Step3ArrayEasy2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#define INT_MIN (-2147483647 - 1)
+using namespace std;
+//Find second largest num in array without sorting ;
+
+/* T.C -> O(2N);
+void SecLargNum(int arr[],int n,int maxVal){
+int secLarg=INT_MIN;
+    for(int i=1;i<n;i++){
+   if(arr[i]>maxVal){
+    maxVal=arr[i];
+   }
+}
+
+for(int i=0;i<=n-1;i++){
+if(arr[i]<maxVal&&arr[i]>secLarg){
+    secLarg=arr[i];
+}
+}
+cout<<secLarg;
+
+}
+*/
+
+void SecLargeNumbetter(int arr[],int n,int maxVal){
+int secLarg=INT_MIN;
+    for(int i=0;i<=n-1;i++){
+        if()
+    }
+}
+
+
+int main(){
+
+
+int min=INT_MIN;
+int arr[]={2,1,6,3,5};
+int n=sizeof(arr)/sizeof(arr[0]);
+int maxVal=arr[0];
+//SecLargNum(arr,n,maxVal);
+
+
+
+    return 0;
+}
