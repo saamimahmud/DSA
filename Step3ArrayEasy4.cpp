af17a594902c9261fae8b19f5359d7800cd13e33
saamimahmud <saamimahmud123@gmail.com>
2025-08-26 22:12:25 +0530

    new commits

--- Step3ArrayEasy4.cpp
+++ Step3ArrayEasy4.cpp
@@ -5,18 +5,21 @@ using namespace std;
 vector<int>  RemDups(vector<int> arr){
 int n= arr.size();
 vector<int> nums;
+int count=0;
 for(int i=0;i<n;i++){
     if(arr[i]==arr[i+1]){
 
     }
     else{
         nums.push_back(arr[i]);
+        count=count+1;
     }
 }
 
 for(int val:nums){
     cout<<val<<" ";
 }
+cout<<count;
 
 }
 
