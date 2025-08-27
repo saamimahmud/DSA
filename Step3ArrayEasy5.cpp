ace4ce221f3fc00a165ceef778003f116a5692c4
saamimahmud <saamimahmud123@gmail.com>
2025-08-27 06:29:44 +0530

    new commits

--- Step3ArrayEasy5.cpp
+++ Step3ArrayEasy5.cpp
@@ -3,21 +3,26 @@
 using namespace std;
 
 //Left rotate an array by one place;
+//T.C-> O(N)
 void leftRot(vector<int> arr){
 int temp=0;
 int n=arr.size();
+temp=arr[0];
 for(int i=0;i<n;i++){
-
+    swap(arr[i],arr[i+1]);
 }
+arr[n-1]=temp;
 
-
+for(int val:arr){
+    cout<<val<<" ";
+}
 
 }
 
 
 int main(){
-
-
+vector<int> arr={1,2,3,4,5};
+leftRot(arr);
 
     return 0;
 }
