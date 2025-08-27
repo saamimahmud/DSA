4dc8021a68be1f8a4296a10b18710dc186db1ce1
saamimahmud <saamimahmud123@gmail.com>
2025-08-27 06:03:00 +0530

    new commits

--- Step3ArrayEasy4.cpp
+++ Step3ArrayEasy4.cpp
@@ -47,11 +47,33 @@ cout<<"Number of elements in the array after del of dupes : "<<count;
 
 }
 
+//Optimal approach using two pointers.
+//T.C -> O(N);
+//f(x) returns the number of unique elements in the array
+void uniqNums(vector<int> arr){
+int n=arr.size();
+int i=0;
+for(int j=1;j<n;j++){
+    if(arr[i]!=arr[j]){
+        arr[i+1]=arr[j];
+        i++;
+    }
+
+}
+for(int j=0;j<=i;j++){
+    cout<<arr[j]<<" ";
+}
+cout<<" The number of elements unique in the array are : "<<i+1;
+}
+
+
 
 int main (){
 vector<int> arr={1,2,2,2,2,3,3,4,4,4,4,4,4};
 RemDups(arr);
 cout<<endl;
 remDupesSet(arr);
+cout<<endl;
+uniqNums(arr);
     return 0;
 }
