1b9ecdb6f115f5f49aa0a8b3f8b90d9914a176fd
saamimahmud <saamimahmud123@gmail.com>
2025-08-27 07:07:25 +0530

    final commits for array rotation in any dir

--- Step3ArrayEasy6.cpp
+++ Step3ArrayEasy6.cpp
@@ -14,7 +14,7 @@ void print(vector<int> arr){
 vector<int> rotArray(vector<int> arr,int k,string direction){
 int n=arr.size();
 vector<int> temp;
-
+cout<<n;
 //for left rotate
 int j=0;
 if(direction == "left"){
@@ -31,12 +31,17 @@ return temp;
 }
 //for right rotate
 else if(direction=="right"){
-
+j==0;
+for(int i=n-k;i<n;i++){
+    temp.push_back(arr[i]);
 }
+while(j<=n-k-1){
+    temp.push_back(arr[j]);
 
-
-
-
+j++;
+}
+return temp;
+}
 }
 
 
@@ -47,8 +52,9 @@ cout<<"Enter the index for rotation of array : "<<endl;
 cin>>k;
 cout<<"Enter the rotation 'right' or 'left' :  "<<endl;
 cin>>direction;
-vector<int> arr={3,7,8,9,10,11};
-
+vector<int> arr={1,2,3,4,5,6,7};
+//N = 6, array[] = {3,7,8,9,10,11} , k=3 , left 
+//N = 7, array[] = {1,2,3,4,5,6,7} , k=2 , right
 print(rotArray(arr,k,direction));
 
     return 0;
