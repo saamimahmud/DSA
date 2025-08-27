92e3375c771bf1e50bbc0b611a96c31abdae5062
saamimahmud <saamimahmud123@gmail.com>
2025-08-27 06:11:30 +0530

    new commits

---
+++ Step3ArrayEasy5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//Left rotate an array by one place;
+void leftRot(vector<int> arr){
+int temp=0;
+int n=arr.size();
+for(int i=0;i<n;i++){
+
+}
+
+
+
+}
+
+
+int main(){
+
+
+
+    return 0;
+}
