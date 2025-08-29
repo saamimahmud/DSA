64e24ff3e2ca212f2388fb62fa3dc77afbe66da4
saamimahmud <saamimahmud123@gmail.com>
2025-08-29 12:35:01 +0530

    new commits

--- Step3ArrayEasy7.cpp
+++ Step3ArrayEasy7.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 //Brute Force approach 
-
+//T.C -> O(2N)
 void moveZeroes(vector<int>& arr)
 {
     int n=arr.size();
@@ -20,10 +20,25 @@ void moveZeroes(vector<int>& arr)
     for(int i=count;i<n;i++){
         arr[i]=0;
     }
-    
+}
+//Optimal approach using two pointers
 
-
-
+void moveZeroesOptimal(vector<int>& arr)
+{
+    int j=0;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            j=i;
+            break;
+        }
+    }
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
 }
 
 
@@ -31,6 +46,11 @@ int main(){
 vector<int> arr={1,2,0,0,3,2,0,2,4};
 
 moveZeroes(arr);
+for(int val:arr){   
+    cout<<val<<" ";
+}
+moveZeroesOptimal(arr);
+cout<<endl;
 for(int val:arr){   
     cout<<val<<" ";
 }
