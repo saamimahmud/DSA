43d0ed528523ce35490353e0318963d21346e44d
saamimahmud <saamimahmud123@gmail.com>
2025-09-03 16:18:20 +0530

    new commits

--- Step3ArrayEasy9.cpp
+++ Step3ArrayEasy9.cpp
@@ -18,7 +18,7 @@ void unionOfArrays(vector<int> &arr1,vector<int> &arr2){
         cout<<val<<" ";
     }
 }
-//Optimal approach using two pointers
+//Optimal approach for Union using two pointers
 //T.C -> O(n1+n2) & S.C-> O(1);
 void unionOfArrays2Pointers(vector <int> &arr1,vector <int> &arr2){
     int n1=arr1.size();
@@ -26,34 +26,30 @@ void unionOfArrays2Pointers(vector <int> &arr1,vector <int> &arr2){
     vector<int> unionArr;
 int i=0,j=0;
     while(i<n1&&j<n2){
-        if(arr1[i]<arr2[j]){
-         if(i<n1)  { 
+        if(arr1[i]<=arr2[j]){
             if(unionArr.empty() || unionArr.back()!=arr1[i])
-            {unionArr.push_back(arr1[i]);}
+            {unionArr.push_back(arr1[i]);
+            }
             i++;
+        
         }
-        }
-        if(arr2[j]<arr1[i]){
-            
-            if(j<n2)
-            {
+        else{
                 if(unionArr.empty()|| unionArr.back()!=arr2[j]){
                 unionArr.push_back(arr2[j]);
             }
-            j++;}
-        }
-     if(arr1[i]==arr2[j]){
-        
-            unionArr.push_back(arr1[i]);
-            i++;
             j++;
-    }}
+        }
+     }
     while(i<n1){
-        unionArr.push_back(arr1[i]);
+        if(unionArr.empty()|| unionArr.back()!=arr2[j])
+        {unionArr.push_back(arr1[i]);
+        }
         i++;
     }
     while(j<n2){
-        unionArr.push_back(arr2[j]);
+        if(unionArr.empty()|| unionArr.back()!=arr2[j])
+       { unionArr.push_back(arr2[j]);
+        }
         j++;
     }
     
@@ -61,15 +57,95 @@ int i=0,j=0;
         cout<<val<<" ";
     }
 }
+//Brute force approach for intersection of two arrays
+void intersectionOfArrays(vector<int> &arr1,vector<int> &arr2){
+vector<int> intersection;
+    int i=0,j=0;
+    int n1=arr1.size();
+    int n2=arr2.size();
+    while(i<n1&&j<n2){
+        
+        for(int k=0;k<n2;k++){
+            
+            if(arr1[i]==arr2[k]){
+                intersection.push_back(arr1[i]);
+                break;
+            }
+        }
+        i++;
+    }
+for(int val:intersection){
+        cout<<val<<" ";
+    }
+
+}
+
+
+//Brute force approach for intersection of two arrays
+//T.C -> O(n1*n2) & S.C-> O(n1)
+void intersectionOfArrays1(vector<int> &arr1,vector<int> &arr2){
+
+vector<int> ans;
+    int i=0,j=0;
+    int n1=arr1.size();
+    int n2=arr2.size();
+    int intersection[n1]={0};
+    
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n2;j++){
+            if(arr1[i]==arr2[j]){// && intersection[j]==0)
+                ans.push_back(arr1[i]);
+                intersection[j]+=1;
+                break;
+            }
+            if(arr2[j]>arr1[i]){
+                break;
+            }
+        }
+    }
+for(int val: ans)  {
+    
+        cout<<val<<" ";
+    
+}
+
+}
+//Optimal approach using two pointers for Intersection of arrays;
+void intersectionUsing2Pointers(vector<int> &arr1,vector<int> &arr2){
+    int n1=arr1.size();
+    int i=0;
+    int j=0;
+    vector<int> intersectionAns;
+
+    while(i<n1){
+        if(arr1[i]<arr2[j]){
+            i++;
+        }
+        else{
+            intersectionAns.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+    for(int val: intersectionAns){
+        cout<<val<<" ";
+    }
+
+}
 
 int main(){
 vector<int> arr1={1,1,2,3,3,4,5};
-vector<int> arr2={2,3,4,5,6,7,8,9};
+vector<int> arr2={2,3,3,4,5,6,7,8,9};
 
     unionOfArrays(arr1,arr2);
     cout<<endl;
     unionOfArrays2Pointers(arr1,arr2);
-    
+    cout<<endl;
+    intersectionOfArrays(arr1,arr2);
+    cout<<endl;
+    intersectionOfArrays1(arr1,arr2);
+    cout<<endl;
+    intersectionUsing2Pointers(arr1,arr2);
     return 0;
 }
 
