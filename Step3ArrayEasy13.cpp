83bc5f17abcd370a803ad3bad6675b273acb04c3
saamimahmud <saamimahmud123@gmail.com>
2025-09-06 22:07:40 +0530

    new commits

---
+++ Step3ArrayEasy13.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+/*
+............................................Longest Subarray with given Sum K(Positives).....................................
+Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+Result: 3
+Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+*/
+//Brute force approach 
+//T.C-> O(n^3);
+//S.C-> O(1)
+void longSubArr(vector<int> &nums,int key){
+int n=nums.size();
+int sum=0;
+int len=0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            sum=0;
+            for(int k=i;k<=j;k++){
+                sum=sum+nums[k];
+            }
+            if (sum==key){
+                len=max(len,j-i+1);
+            }
+        }
+    }
+    cout<<len;
+}
+//Brute fore with one less loop;
+//T.C-> O(n^2);
+//S.C(1);
+void longSubArr1(vector<int> &nums,int key){
+int n=nums.size();
+int sum=0;
+int len=0;
+    for(int i=0;i<n;i++){
+        sum=0;
+        for(int j=i;j<n;j++){
+            sum=sum+nums[j];
+           if(sum==key){
+            len=max(len,j-i+1);
+        }
+        }
+        
+    }
+    cout<<len;
+}
+
+
+int main(){
+int size;
+cin>>size;
+vector<int> nums;
+int q;
+while(size--){
+    cin>>q;
+    nums.push_back(q);
+}
+//{2,3,5,1,9};
+int key=10;
+longSubArr(nums,key);
+longSubArr1(nums,key);
+
+    return 0;
+}
