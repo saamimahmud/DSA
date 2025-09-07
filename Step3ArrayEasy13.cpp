96ab5a255c428f0969ef61aa638929c954fb122d
saamimahmud <saamimahmud123@gmail.com>
2025-09-07 20:55:25 +0530

    new commit

--- Step3ArrayEasy13.cpp
+++ Step3ArrayEasy13.cpp
@@ -27,7 +27,7 @@ int len=0;
             }
         }
     }
-    cout<<len;
+    cout<<"The longest subarray is of length : "<<len<<endl;
 }
 //Brute fore with one less loop;
 //T.C-> O(n^2);
@@ -46,37 +46,56 @@ int len=0;
         }
         
     }
-    cout<<len;
+    cout<<"The longest subarray is of length : "<<len<<endl;
 }
 
 //Better approach using HashMap;
 
-void longSubArr2(vector<int> arr,long long k){
+int longSubArr2(vector<int> arr,long long k){
 int n=arr.size();
 map <long long, int> hashMap;
-int len=0;
+int maxLen=0;
 long long sum=0;
+long long rem=0;
 
 for(int i=0;i<n;i++){
 sum=sum+arr[i];
+if(sum==k){
+    maxLen=max(maxLen,i+1);
 }
+rem=sum-k;
+if(hashMap.find(rem)!=hashMap.end()){
+   int len= i-hashMap[rem];
+   maxLen=max(maxLen,len);
+}
+if(hashMap.find(sum)==hashMap.end()){//this means that the sum is not in hash map; this helps in case we have zeroes in the aray.....
+hashMap[sum]=i;
+}
+
+
+}
+return maxLen;
 
 }
 
 
 int main(){
 int size;
+cout<<" Enter the size of your array : "<<endl;
 cin>>size;
 vector<int> nums;
 int q;
+cout<<"Enter the elements of the array one by one : "<<endl;
 while(size--){
     cin>>q;
     nums.push_back(q);
 }
-//{2,3,5,1,9};
-int key=10;
+//{2,-2,1,0,3};
+cout<<"Enter the key : "<<endl;
+int key;
+cin>>key;
 longSubArr(nums,key);
 longSubArr1(nums,key);
-
+cout<<"The longest subarray is of length : "<<longSubArr2(nums,key);
     return 0;
 }
