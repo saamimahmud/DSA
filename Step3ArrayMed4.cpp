0cd555c2cb05a8f60a01134140a2e9e82baa5aae
saamimahmud <saamimahmud123@gmail.com>
2025-09-17 00:15:03 +0530

    added better and Optimal approaches

---
+++ Step3ArrayMed4.cpp
@@ -0,0 +1,53 @@
+//Kadane's Algorithm : Maximum Subarray Sum in an Array
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+//Question : Given an integer array arr, find the contiguous subarray (containing at least one number) which
+//has the largest sum and returns its sum and prints the subarray.
+
+
+//Brute Force approach is T.C-> O(N*N*N)
+// and this code is better with
+//T.C-> O(N*N)
+void kadane_1 (vector<int> &nums){
+    int n=nums.size();
+    int sum=0;
+    int maxSum=0;
+    for(int i=0;i<n;i++){
+        sum=0;
+        for(int j=i;j<n;j++){
+            sum=sum+nums[j];
+            maxSum=max(sum,maxSum);
+        }
+
+    }
+    cout<<maxSum;
+}
+
+//Optimal approach using Kadane's Algorithm
+//T.C->O(n)...
+//S.C->O(1)...
+void kadaneOPT(vector<int> &nums){
+   int n=nums.size();
+    int sum=nums[0];
+    int maxSum=nums[0];
+
+for(int i=0;i<n;i++){
+    sum=max(nums[i],sum+nums[i]);
+    maxSum=max(sum,maxSum);
+}
+cout<< maxSum;
+}
+
+
+
+
+
+int main (){
+vector<int> arr={-3,-2,-2,-3} ;
+//kadane_1(arr);
+kadaneOPT(arr);
+
+    return 0;
+}
