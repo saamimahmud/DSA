16f3736533782ac93d93f036f57f94d20959ad2e
saamimahmud <saamimahmud123@gmail.com>
2025-09-27 03:12:09 +0530

    added the most optimal solution for  3Sum

--- Step3ArrayHard3.cpp
+++ Step3ArrayHard3.cpp
@@ -58,6 +58,47 @@ int third=0;
     return res;
 }
 
+//Best approach. using 2 pointers
+//T.C-> O(nlog(n)+O(N*N))
+//S.C-> O(no. of unique elems). we do not include the list of vectors because we are using it to just return the answer ..
+vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> list;
+    sort(nums.begin(),nums.end());
+    int j;
+    int k;
+       for(int i=0;i<n;i++){
+        j=i+1;
+        k=nums.size()-1;
+        if(i>0&&nums[i]==nums[i-1]){
+            continue;
+        }
+        while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum>0){
+                k--;
+            }
+            else if (sum<0){
+                j++;
+            }
+            else{
+                vector<int> temp={nums[i],nums[j],nums[k]};
+                
+               list.push_back(temp);
+                j++;
+                k--;
+                while(j<k&&nums[j]==nums[j-1]){
+                    j++;
+                }
+                while(j<k&&nums[k]==nums[k+1])
+                k--;
+            }
+        }
+       }
+       
+    return list;
+    }
+
 
 
     int main(){
