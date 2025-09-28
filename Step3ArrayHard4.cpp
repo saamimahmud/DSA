4121e8fa5db55b58ffe99c24736baa919fc1446f
saamimahmud <saamimahmud123@gmail.com>
2025-09-28 21:34:16 +0530

    new commits

--- Step3ArrayHard4.cpp
+++ Step3ArrayHard4.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
 using namespace std;
 
 
 
-void fourSum(){
+void fourSum(vector<int> &nums){
 
+int n=nums.size();
+    for (int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;j<n;j++){
+                for(int l=k+1;l<n;l++){
+                    int sum =nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==0){
+                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+                        sort(temp.begin(),temp.end());
+                    }
+                }
+               
+            }
+        }
+    }
 }
 
 int main(){
