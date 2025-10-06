5827170d04d984aa9e38859dbeebaf2601f7d40a
saamimahmud <saamimahmud123@gmail.com>
2025-10-07 01:38:32 +0530

    updated the optimal solution for 4Sum

--- Step3ArrayHard4.cpp
+++ Step3ArrayHard4.cpp
@@ -57,10 +57,57 @@ using namespace std;
         return res;
     }
 
+//Optimal Solution..
+//T.C-> Near O(N^3)
+//S.C-> O(No. of quads).... Space is used only to return the solution and not solve the problem....
+    vector<vector<int>> fourSum2(vector<int> &nums,int target){
+int n=nums.size();
+vector<vector<int>> ans;
+sort(nums.begin(),nums.end());
+for(int i=0;i<n;i++){
+if(i>0 && nums[i]==nums[i-1]){
+            continue;
+        }
+    for(int j=i+1;j<n;j++){
+        if(j>i+1 && nums[j]==nums[j-1]){
+            continue;
+        }
+    int k=j+1;
+    int l=n-1;
+    while(k<l) { 
+        long long sum=nums[i];
+        sum+=nums[j];
+        sum+=nums[k];
+        sum+=nums[l];
+        if(sum==target){
+            vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
+            ans.push_back(temp);
+            k++;
+            l--;
+        }  
+        else if(sum<target){
+            k++;
+        }
+        else{
+            l--;
+        }
+
+        while(k<l && nums[k]==nums[k+1]){
+            k++;
+        }
+        while(k<l && nums[l]==nums[l+1]){
+            l--;
+        }
+}
+    }
+}
+return ans;
+    }
+
 int main(){
 vector<int> arr={1, 0, -1, 0, -2, 2};
 int target=0;
-vector<vector<int>> result=fourSum(arr,target);
+vector<vector<int>> result=fourSum2(arr,target);
 for(const vector<int> &list:result){
     for(int val:list){
         cout<<val<<" ";
