de79b6112a01404752812765f3d36e1a75fe1700
saamimahmud <saamimahmud123@gmail.com>
2025-10-09 00:43:54 +0530

    intial commits

---
+++ Step3ArrayHard5.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<vector<int>> func(vector<int> &nums){
+    int n=nums.size();
+    vector<vector<int>> result;
+    int maxLen=0;
+int sum = 0;
+    for(int i=0;i<n;i++){
+        sum=0;
+        for(int j=i;j<n;j++){
+            
+        sum+=nums[j];
+            
+            if(sum==0){
+                vector<int> temp;
+           for(int k=i;k<=j;k++){
+            temp.push_back(nums[k]);
+            
+           }
+           result.push_back(temp);
+        }
+        
+        
+        }
+       
+        
+    }
+    return result;
+}
+
+int main(){
+vector<int> arr={6, -2, 2, -8, 1, 7, 4, -10};
+vector<vector<int>> res=func(arr);
+
+for(const vector<int> &list:res){
+    for(int val:list){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+}
+
+    return 0;
+}
